005f5b88e1dbd9d3eb074d09fae03a5289143473
guilhermeluizstolfo <guilhermeluizstolfo@10.1.1.2>
2016-08-28 13:54:02 -0300

    Criação do usuario padrao do desafioConcrete

--- src/main/java/br/com/concretesolutions/spring/CreateTablesInMemory.java
+++ src/main/java/br/com/concretesolutions/spring/CreateTablesInMemory.java
@@ -2,10 +2,11 @@ package br.com.concretesolutions.spring;
 
 import org.hibernate.Session;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import br.com.concretesolutions.beans.Login;
-import br.com.concretesolutions.beans.Register;
-import br.com.conretesolutions.database.hibernate.Hibernate;
+import br.com.concretesolutions.uuid.UUIDI;
+import br.com.conretesolutions.hibernate.Hibernate;
 
 /**
  * Create tables in memory
@@ -17,13 +18,22 @@ public class CreateTablesInMemory implements InitializingBean{
 	
 	Hibernate hibernate = new Hibernate();
 	
+	@Autowired
+	UUIDI uuidCreate; 
+	
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		Session session = hibernate.getSessionFactory();
 		
-		session.beginTransaction();
+		Login login = new Login();
+			
+		login.setId(uuidCreate.getUUID());
+		login.setEmail("stolfo@gmail.com");
+		login.setPassword("123456");
 		
-		hibernate.commit();
+		session.save(login);		
+		
+		session.beginTransaction().commit();
 		
 		session.close();	
 	}
